5fc3e784664736fcd4c6f0c3c81b02949ccf2fa8
Ethan Harlig <ethan.harlig@gmail.com>
2016-11-06 23:13:34 -0800

    Added readme and example credentials file

---
+++ README.md
@@ -0,0 +1,2 @@
+Setup:
+Please be sure to add a password.txt file with line 0 as the email address to send from and line 1 as the password of that email address. See example password.txt
--- sendMailToNick.py
+++ sendMailToNick.py
@@ -4,10 +4,10 @@ import sched, time
 import random
 import datetime
 
-def send_email():
-    server.sendmail("iloveanimals69420@gmail.com", "2094068179@vtext.com", msg) # random (geoff)
-    server.sendmail("iloveanimals69420@gmail.com", "8185994321@mms.att.net", msg) # shayan
-    server.sendmail("iloveanimals69420@gmail.com", "9253896304@mms.att.net", msg) # pledge
+def send_email(username):
+    server.sendmail(username, "2094068179@vtext.com", msg) # random (geoff)
+    server.sendmail(username, "8185994321@mms.att.net", msg) # shayan
+    server.sendmail(username, "9253896304@mms.att.net", msg) # pledge
     print('Sent: %s\n' % msg)
 
 with open('comein.txt') as f:
@@ -19,21 +19,22 @@ for p in pre:
 
 with open('password.txt') as f:
     content = f.readlines()
-password = content[0].rstrip('\n')
+username = content[0].rstrip('\n')
+password = content[1].rstrip('\n')
 
 while 1:
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login("iloveanimals69420@gmail.com", password)
+    server.login(username, password)
 
     my_int = int(random.random() * (len(animals) - 1));
     msg = "Need a cute pic of " + animals[my_int]
 
-    minute_delay = 10 # number of minutes between each text (approx because server.sendmail takes a while)
+    minute_delay = 0 # number of minutes between each text (approx because server.sendmail takes a while)
 
     s = sched.scheduler(time.time, time.sleep(60 * minute_delay))
 
     print (datetime.datetime.now())
-    s.enter(minute_delay, 1, send_email(), None)
+    s.enter(minute_delay, 1, send_email(username), None)
 
     server.quit()
